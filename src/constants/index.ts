c308623f764e2474ee3df1eecd0bf167743fb571
MxAziz <hereabdulaziz@gmail.com>
2025-10-05 15:54:04 +0600

    add transaction type and transaction status constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,17 @@ export const role = {
   user: "USER",
   agent: "AGENT",
 };
+
+export const transactionTypes = [
+  { value: "ADD_MONEY", label: "Add Money" },
+  { value: "WITHDRAW_MOENY", label: "Withdraw" },
+  { value: "SEND_MONEY", label: "Send Money" },
+  { value: "CASH_IN", label: "Cash In" },
+  { value: "CASH_OUT", label: "Cash Out" },
+];
+
+export const transactionStatus = [
+  { value: "PENDING", label: "Pending" },
+  { value: "COMPLETED", label: "Completed" },
+  { value: "REVERSED", label: "Reversed" },
+];
