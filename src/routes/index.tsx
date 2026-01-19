7f8409d0426fd7e8004ffd6941e433558c432a07
MxAziz <hereabdulaziz@gmail.com>
2026-01-19 16:56:25 +0600

    update footer and added some valid link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,79 +23,85 @@ import About from "@/pages/About";
 import Features from "@/pages/Features";
 
 export const router = createBrowserRouter([
-  {
-    Component: App,
-    path: "/",
-    children: [
-      {
-        index: true,
-        Component: Home,
-      },
-      {
-        Component: About,
-        path: "/about"
-      },
-      {
-        Component: Features,
-        path: "/features"
-      },
-      {
-        Component: Contact,
-        path: "/contact",
-      },
-      {
-        Component: FAQ,
-        path: "/faq",
-      },
-    ],
-  },
-  { path: "/login", Component: Login },
-  { path: "/register", Component: Register },
-    { Component: UnauthorizedPage, path: "/unauthorized" },
-  { Component: NotFound, path: "*" },
+     {
+          Component: App,
+          path: "/",
+          children: [
+               {
+                    index: true,
+                    Component: Home,
+               },
+               {
+                    Component: About,
+                    path: "/about",
+               },
+               {
+                    Component: Features,
+                    path: "/features",
+               },
+               {
+                    Component: Contact,
+                    path: "/contact",
+               },
+               {
+                    Component: FAQ,
+                    path: "/faq",
+               },
+          ],
+     },
+     { path: "/login", Component: Login },
+     { path: "/register", Component: Register },
+     { Component: UnauthorizedPage, path: "/unauthorized" },
+     { Component: NotFound, path: "*" },
 
-  {
-    Component: checkAuth(DashboardLayout, ...(Object.values(role) as [TRole])),
-    path: "/my-wallet",
-    children: [
-      ...generateRoutes(
-        ...UserAgentCommonSidebar,
-        ...UserSidebar,
-        ...AgentSidebar
-      ),
-    ],
-  },
+     {
+          Component: checkAuth(
+               DashboardLayout,
+               ...(Object.values(role) as [TRole]),
+          ),
+          path: "/my-wallet",
+          children: [
+               ...generateRoutes(
+                    ...UserAgentCommonSidebar,
+                    ...UserSidebar,
+                    ...AgentSidebar,
+               ),
+          ],
+     },
 
-  {
-    Component: checkAuth(DashboardLayout, ...(Object.values(role) as [TRole])),
-    path: "/dashboard",
-    children: [
-      { index: true, element: <Navigate to="/dashboard/overview" /> },
-      ...generateRoutes(...AdminSidebar),
-      {
-        Component: checkAuth(
-          SingleUser,
-          role.admin as TRole,
-          role.superAdmin as TRole
-        ),
-        path: "/dashboard/agents/:id",
-      },
-      {
-        Component: checkAuth(
-          SingleWallet,
-          role.admin as TRole,
-          role.superAdmin as TRole
-        ),
-        path: "/dashboard/wallets/:id",
-      },
-      {
-        Component: checkAuth(
-          SingleTransaction,
-          role.admin as TRole,
-          role.superAdmin as TRole
-        ),
-        path: "/dashboard/transactions/:id",
-      },
-    ],
-  },
+     {
+          Component: checkAuth(
+               DashboardLayout,
+               ...(Object.values(role) as [TRole]),
+          ),
+          path: "/dashboard",
+          children: [
+               { index: true, element: <Navigate to="/dashboard/overview" /> },
+               ...generateRoutes(...AdminSidebar),
+               {
+                    Component: checkAuth(
+                         SingleUser,
+                         role.admin as TRole,
+                         role.superAdmin as TRole,
+                    ),
+                    path: "/dashboard/agents/:id",
+               },
+               {
+                    Component: checkAuth(
+                         SingleWallet,
+                         role.admin as TRole,
+                         role.superAdmin as TRole,
+                    ),
+                    path: "/dashboard/wallets/:id",
+               },
+               {
+                    Component: checkAuth(
+                         SingleTransaction,
+                         role.admin as TRole,
+                         role.superAdmin as TRole,
+                    ),
+                    path: "/dashboard/transactions/:id",
+               },
+          ],
+     },
 ]);
